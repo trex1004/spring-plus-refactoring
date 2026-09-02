657f700db4869c6106cb4dc2ef19b1a4925cb9ab
trex1004 <trexs@gmail.com>
2026-09-02 22:57:34 +0900

    Level 2 - refactor: Todo 단건 조회를 QueryDSL로 전환
    - QueryDSL 7.6과 Jakarta annotation processor를 추가
    - JPAQueryFactory와 Todo Query Repository Fragment를 구성
    - Todo 단건 조회 JPQL을 QueryDSL fetch join으로 교체
    - Todo와 작성자 정보를 한 번의 조회로 가져오도록 변경

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepositoryImpl.java
@@ -0,0 +1,26 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.Todo;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+import static org.example.expert.domain.user.entity.QUser.user;
+
+@RequiredArgsConstructor
+public class TodoQueryRepositoryImpl implements TodoQueryRepository {
+private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        Todo result = jpaQueryFactory
+                .selectFrom(todo)
+                .join(todo.user, user).fetchJoin()
+                .where(todo.id.eq(todoId))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+}
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,9 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryRepository {
 
     @Query(
             value = """
@@ -34,8 +33,5 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
                            @Param("modifiedAtEnd") LocalDateTime modifiedAtEnd,
                            Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
 }
